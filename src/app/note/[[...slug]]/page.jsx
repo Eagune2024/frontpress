843b086a211a5350eaff45fa4ed8688df1449862
Eagune2024 <jingdiqkl@163.com>
2024-11-12 23:15:13 +0800

    feat: remove Suspense

--- src/app/note/[[...slug]]/page.jsx
+++ src/app/note/[[...slug]]/page.jsx
@@ -38,21 +38,19 @@ export default async function HomeNoteLayout ({ params }) {
       <div className="flex flex-1 border border-black m-4 rounded-lg overflow-hidden">
         <div className="h-full flex flex-col border-r border-black w-72">
           <ScrollArea className="flex-1 flex flex-col gap-4 py-2 border-r">
-            <Suspense fallback={<div className="m-2">Loading...</div>}>
-              <nav className="grid gap-1 px-2">
-                {
-                  booklist.map((book) => (
-                    <Link key={book.id} href={`/note/${book.id}`}>
-                      <Button variant={`${book.id}` === currentBookId ? '' : 'ghost'} className="h-8 w-full" >
-                        <FileTextIcon className="mr-1 h-4 w-4" />
-                        { book.name }
-                        <span className="ml-auto dark:text-white">{ book.note }</span>
-                      </Button>
-                    </Link>
-                  ))
-                }
-              </nav>
-            </Suspense>
+            <nav className="grid gap-1 px-2">
+              {
+                booklist.map((book) => (
+                  <Link key={book.id} href={`/note/${book.id}`}>
+                    <Button variant={`${book.id}` === currentBookId ? '' : 'ghost'} className="h-8 w-full" >
+                      <FileTextIcon className="mr-1 h-4 w-4" />
+                      { book.name }
+                      <span className="ml-auto dark:text-white">{ book.note }</span>
+                    </Button>
+                  </Link>
+                ))
+              }
+            </nav>
           </ScrollArea>
         </div>
         <div className="h-full flex flex-col border-r border-black w-80">
