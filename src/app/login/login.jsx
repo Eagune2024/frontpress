d1c7893fe53f5c1e01346000629c1d4fa36538ee
huyijin <huyijin@onepro.cloud>
2024-11-13 17:44:41 +0800

    feat: build login直接跳转

---
+++ src/app/login/login.jsx
@@ -0,0 +1,32 @@
+import LoginForm from "./form"
+
+export default function LoginView () {
+  return (
+    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          FrontPress
+        </div>
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">连接创意</p>
+            <footer className="text-sm">瞬间启动，无缝编码</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              登陆
+            </h1>
+          </div>
+          <div className="w-[400px] h-[300px]">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
