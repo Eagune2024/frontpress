d49054771c51a111ede930620d6b2762346e33f6
huyijin <huyijin@onepro.cloud>
2024-11-13 09:50:07 +0800

    feat: note page 展示

--- src/app/note/[[...slug]]/page.jsx
+++ src/app/note/[[...slug]]/page.jsx
@@ -1,37 +1,36 @@
 import HomeLayout from '@/app/layout/homeLayout'
-import { Suspense } from "react";
 import { ScrollArea } from "@/ui/scroll-area"
 import supabase from "@/utils/supabaseClient";
-import { unstable_cache } from 'next/cache'
 import { Button } from "@/ui/button"
-import { FileTextIcon } from "@radix-ui/react-icons"
+import { FileTextIcon, FileIcon, Pencil2Icon } from "@radix-ui/react-icons"
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
 export async function generateStaticParams() {
-  const { data: booklist, error } = await getBookList()
-  return [{ 'slug': undefined }].concat(booklist.map((book) => ({ 'slug': [`${book.id}`] })))
+  const { data: booklist, error } = await supabase.from('Notebook').select('name, id, created_at')
+  const noteResList = await Promise.all(booklist.map((book) => (supabase.from('Note').select('name, id, created_at').eq('notebook_id', book.id))))
+  return noteResList.reduce((params, { data: notelist }, index) => {
+    return params.concat([{ 'slug': [`${booklist[index].id}`] }]).concat(notelist.map((note) => ({ 'slug': [`${booklist[index].id}`, `${note.id}`] })))
+  }, [{ 'slug': undefined }])
 }
-
-const getBookList = unstable_cache(
-  async () => {
-    return await supabase.from('Notebook').select('name, id, created_at')
-  },
-  ['BookList'],
-  { revalidate: 60, tags: ['BookList'] }
-)
 
 export default async function HomeNoteLayout ({ params }) {
   const { slug } = await params
-  const { data: booklist, error } = await getBookList()
+  const { data: booklist, errorNoteBook } = await supabase.from('Notebook').select('name, id, created_at')
 
   if (!slug && booklist.length) {
     redirect(`/note/${booklist[0].id}`)
   }
 
   const [currentBookId, currentNoteId] = slug
-  
-  console.log(currentBookId, currentNoteId)
+  const { data: notelist, errorNote } = await supabase.from('Note').select('name, id, created_at').eq('notebook_id', currentBookId)
+
+  if (!currentNoteId && notelist.length) {
+    redirect(`/note/${currentBookId}/${notelist[0].id}`)
+  }
+
+  const { data } = await supabase.from('Note').select('name, id, content, created_at').eq('id', currentNoteId).maybeSingle()
+  const note = data || {}
 
   return (
     <HomeLayout>
@@ -42,7 +41,7 @@ export default async function HomeNoteLayout ({ params }) {
               {
                 booklist.map((book) => (
                   <Link key={book.id} href={`/note/${book.id}`}>
-                    <Button variant={`${book.id}` === currentBookId ? '' : 'ghost'} className="h-8 w-full" >
+                    <Button variant={ `${book.id}` === currentBookId ? '' : 'ghost'} className="h-8 w-full" >
                       <FileTextIcon className="mr-1 h-4 w-4" />
                       { book.name }
                       <span className="ml-auto dark:text-white">{ book.note }</span>
@@ -54,9 +53,26 @@ export default async function HomeNoteLayout ({ params }) {
           </ScrollArea>
         </div>
         <div className="h-full flex flex-col border-r border-black w-80">
+          <ScrollArea className="flex-1 flex flex-col gap-4 py-2">
+            <nav className="grid gap-1 px-2">
+              {
+                notelist.map((note, index) => (
+                  <Link key={note.id} href={`/note/${currentBookId}/${note.id}`}>
+                    <Button variant={ `${note.id}` === currentNoteId ? '': 'ghost' } className="h-8 justify-start w-full">
+                      <FileIcon className="mr-2 h-6 w-6"/>
+                      { note.name }
+                    </Button>
+                  </Link>
+                ))
+              }
+            </nav>
+          </ScrollArea>
         </div>
         <div className="flex-1">
-          <div className="border-b border-solid border-black h-14 flex items-center pl-4"></div>
+          <div className="border-b border-solid border-black h-14 flex items-center pl-4">
+            { note.name }
+            <Button variant="link"><Pencil2Icon className="mr-2"/></Button>
+          </div>
         </div>
       </div>
     </HomeLayout>
