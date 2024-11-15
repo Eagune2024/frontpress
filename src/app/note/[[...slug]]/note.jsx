859ca8942a3a6f0b72ac72278ea244f2c0d072ec
huyijin <huyijin@onepro.cloud>
2024-11-15 16:07:58 +0800

    feat: 删除session相关

--- src/app/note/[[...slug]]/note.jsx
+++ src/app/note/[[...slug]]/note.jsx
@@ -2,16 +2,12 @@
 
 import { Button } from "@/ui/button"
 import { Pencil2Icon } from "@radix-ui/react-icons"
-import { useAppContext } from "@/app/appContext";
 
 export default function Note ({ note }) {
-  const { session } = useAppContext()
-
   return (
     <>
       <div className="border-b border-solid border-black h-14 flex items-center pl-4">
         { note.name }
-        { session && <Button variant="link"><Pencil2Icon className="mr-2"/></Button> }
       </div>
     </>
   )
--- src/app/note/[[...slug]]/notelist.jsx
+++ src/app/note/[[...slug]]/notelist.jsx
@@ -4,14 +4,10 @@ import { ScrollArea } from "@/ui/scroll-area"
 import { Button } from "@/ui/button"
 import { FileIcon } from "@radix-ui/react-icons"
 import Link from 'next/link';
-import { useAppContext } from "@/app/appContext";
 
 export default function NoteList ({ currentBookId, currentNoteId, notelist }) {
-  const { session } = useAppContext()
-
   return (
     <>
-      { session && <div className="border-b border-solid border-black h-14"></div> }
       <ScrollArea className="flex-1 flex flex-col gap-4 py-2">
         <nav className="grid gap-1 px-2">
           {
